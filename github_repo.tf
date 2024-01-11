635d3c1642795d8c0ed5296c0ac7cc7c8b784d72
Arif.dev.ops <reza.jmi.ece@gmail.com>
2024-01-11 20:01:31 +0000

    This repo is created by terraform

---
+++ github_repo.tf
@@ -0,0 +1,11 @@
+provider "github" {
+	token = "" 
+  }
+
+  resource "github_repository" "Terraform" {
+	name        = "Terraform_repo"
+	description = "This repo is created through terraform"
+  
+	visibility = "public"
+    auto_init = true
+
